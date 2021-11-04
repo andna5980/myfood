31ad2ccab3a679001758fb8f38ed7b5ac07ddea8
andna5980 <andy5980@hotmail.com>
2021-11-04 13:04:51 +0000

    modal added to delete button

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,11 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "left"});
     $('.collapsible').collapsible();
     $('select').formSelect();
+
+
+    // Materialize modals
+    $('.modal').modal();
     
   });
+
+  
